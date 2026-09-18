df463a446caa0d642a2ff403578b7b082aa23078
ciclosm <davidms281104@gmail.com>
2026-09-18 11:23:41 +0200

    acceso a datos

---
+++ Acesso a datos/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+}
